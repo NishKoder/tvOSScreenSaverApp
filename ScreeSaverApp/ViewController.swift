8ae1332e097f2a384672dcc659865d9d25d2aaa4
Nishant Gupta <shub002gupta@gmail.com>
2020-05-13 01:06:25 +0530

    Initial Commit

---
+++ ScreeSaverApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScreeSaverApp
+//
+//  Created by Nishant Gupta on 13/05/20.
+//  Copyright © 2020 Nishant Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
